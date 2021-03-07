c14cb1485ca5554e94bed4fa277dd349941d6e70
AsyaDoga <asya133@gmail.com>
2021-03-07 16:53:48 +0300

    First

---
+++ .build/obj_kb_asya/common/eeconfig.d
@@ -0,0 +1,11 @@
+.build/obj_kb_asya/common/eeconfig.o: tmk_core/common/eeconfig.c \
+ keyboards/kb/config.h quantum/config_common.h tmk_core/common/eeprom.h \
+ tmk_core/common/eeconfig.h
+
+keyboards/kb/config.h:
+
+quantum/config_common.h:
+
+tmk_core/common/eeprom.h:
+
+tmk_core/common/eeconfig.h:
